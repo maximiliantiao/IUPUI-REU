aeff7eaeaa8c2596826f9efa8125034466066476
Maximilian Tiao <maximilian.tiao@gmail.com>
2021-07-27 16:44:18 -0700

    Updated main_fed.py

--- Federated Learning/main_fed.py
+++ Federated Learning/main_fed.py
@@ -2,93 +2,37 @@
 # -*- coding: utf-8 -*-
 # Python version: 3.6
 
-from models.test import test_img
-from models.Fed import FedAvg
-from models.Nets import *
-from models.Update import LocalUpdate
-from utils.options import args_parser
-from utils.sampling import mnist_iid, mnist_noniid, cifar_iid
-import torch
-from torchvision import datasets, transforms
-import numpy as np
 import copy
+import numpy as np
+from torchvision import datasets, transforms
+import torch
+from utils.sampling import mnist_iid, mnist_noniid, cifar_iid
+from utils.options import args_parser
+from models.Update import LocalUpdate
+from models.Nets import MLP, CNNMnist, CNNCifar
+from models.Fed import FedAvg
+from models.test import test_img
 
-def train(net_glob, args, dataset_train):
-    net_glob.train()
-
-    # copy weights
-    w_glob = net_glob.state_dict()
-
-    # training
-    loss_train = []
-    cv_loss, cv_acc = [], []
-    val_loss_pre, counter = 0, 0
-    net_best = None
-    best_loss = None
-    val_acc_list, net_list = [], []
-
-    if args.all_clients:
-        print("Aggregation over all clients")
-        w_locals = [w_glob for i in range(args.num_users)]
-    for iter in range(args.epochs):
-        loss_locals = []
-        if not args.all_clients:
-            w_locals = []
-        m = max(int(args.frac * args.num_users), 1)
-        idxs_users = np.random.choice(range(args.num_users), m, replace=False)
-        for idx in idxs_users:
-            local = LocalUpdate(
-                args=args, dataset=dataset_train, idxs=dict_users[idx])
-            w, loss = local.train(net=copy.deepcopy(net_glob).to(args.device))
-            if args.all_clients:
-                w_locals[idx] = copy.deepcopy(w)
-            else:
-                w_locals.append(copy.deepcopy(w))
-            loss_locals.append(copy.deepcopy(loss))
-        # update global weights
-        w_glob = FedAvg(w_locals)
-
-        # copy weight to net_glob
-        net_glob.load_state_dict(w_glob)
-
-        # print loss
-        loss_avg = sum(loss_locals) / len(loss_locals)
-        print('Round {:3d}, Average loss {:.3f}'.format(iter, loss_avg))
-        loss_train.append(loss_avg)
-
-def test(net_glob, args, dataset_train, dataset_test):
-    net_glob.eval()
-    acc_train, loss_train = test_img(net_glob, dataset_train, args)
-    acc_test, loss_test = test_img(net_glob, dataset_test, args)
-    print("Training accuracy: {:.2f}".format(acc_train))
-    print("Testing accuracy: {:.2f}".format(acc_test))
 
 if __name__ == '__main__':
-    # Parse args ##################################################################################
+    # Parse args ###################################################################################
     args = args_parser()
-    args.device = torch.device('cuda:{}'.format(
-        args.gpu) if torch.cuda.is_available() and args.gpu != -1 else 'cpu')
+    args.device = torch.device('cuda:{}'.format(args.gpu) if torch.cuda.is_available() and args.gpu != -1 else 'cpu')
 
-    # Load dataset and split users ################################################################
+    # Load clean dataset and split users ###########################################################
     if args.dataset == 'mnist':
-        trans_mnist = transforms.Compose(
-            [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
-        dataset_train = datasets.MNIST(
-            '../data/mnist/', train=True, download=True, transform=trans_mnist)
-        dataset_test = datasets.MNIST(
-            '../data/mnist/', train=False, download=True, transform=trans_mnist)
+        trans_mnist = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
+        dataset_train = datasets.MNIST('../data/mnist/', train=True, download=True, transform=trans_mnist)
+        dataset_test = datasets.MNIST('../data/mnist/', train=False, download=True, transform=trans_mnist)
         # sample users
         if args.iid:
             dict_users = mnist_iid(dataset_train, args.num_users)
         else:
             dict_users = mnist_noniid(dataset_train, args.num_users)
     elif args.dataset == 'cifar':
-        trans_cifar = transforms.Compose(
-            [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
+        trans_cifar = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
         dataset_train = datasets.ImageFolder('./cifar10_pngs/train', transform=trans_cifar)
         dataset_test = datasets.ImageFolder('./cifar10_pngs/test', transform=trans_cifar)
-
         if args.iid:
             dict_users = cifar_iid(dataset_train, args.num_users)
         else:
@@ -106,36 +50,113 @@ if __name__ == '__main__':
         len_in = 1
         for x in img_size:
             len_in *= x
-        net_glob = MLP(dim_in=len_in, dim_hidden=200,
-                       dim_out=args.num_classes).to(args.device)
-    # elif args.model == 'resnet18':
-    #     net_glob = ResNet18().to(args.device)
+        net_glob = MLP(dim_in=len_in, dim_hidden=200, dim_out=args.num_classes).to(args.device)
     else:
         exit('Error: unrecognized model')
-    # print(net_glob)
+    print(net_glob)
+    
+    # Training Clean ##############################################################################
+    net_glob.train()
+    # copy weights
+    w_glob = net_glob.state_dict()
+    loss_train = []
+    cv_loss, cv_acc = [], []
+    val_loss_pre, counter = 0, 0
+    net_best = None
+    best_loss = None
+    val_acc_list, net_list = [], []
 
-    # Train #######################################################################################
-    train(net_glob, args, dataset_train)
+    if args.all_clients: 
+        print("Aggregation over all clients")
+        w_locals = [w_glob for i in range(args.num_users)]
+    for iter in range(args.epochs):
+        loss_locals = []
+        if not args.all_clients:
+            w_locals = []
+        m = max(int(args.frac * args.num_users), 1)
+        idxs_users = np.random.choice(range(args.num_users), m, replace=False)
+        for idx in idxs_users:
+            local = LocalUpdate(args=args, dataset=dataset_train, idxs=dict_users[idx])
+            w, loss = local.train(net=copy.deepcopy(net_glob).to(args.device))
+            if args.all_clients:
+                w_locals[idx] = copy.deepcopy(w)
+            else:
+                w_locals.append(copy.deepcopy(w))
+            loss_locals.append(copy.deepcopy(loss))
+        # update global weights
+        w_glob = FedAvg(w_locals)
 
-    # Test ########################################################################################
-    test(net_glob, args, dataset_train, dataset_test)
+        # copy weight to net_glob
+        net_glob.load_state_dict(w_glob)
 
-    if args.poison == 'True':
-        print("Execute backdoor trigger attack")
+        # print loss
+        loss_avg = sum(loss_locals) / len(loss_locals)
+        print('Round {:3d}, Average loss {:.3f}'.format(iter, loss_avg))
+        loss_train.append(loss_avg)
 
-        trans_cifar = transforms.Compose(
-            [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    # Testing Clean ###############################################################################
+    net_glob.eval()
+    acc_train, loss_train = test_img(net_glob, dataset_train, args)
+    acc_test, loss_test = test_img(net_glob, dataset_test, args)
+    print("Training accuracy: {:.2f}".format(acc_train))
+    print("Testing accuracy: {:.2f}".format(acc_test))
 
+    # Load poisoned dataset and split users #######################################################
+    print("Execute backdoor attack")
+    if args.dataset == 'cifar':
+        trans_cifar = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
         dataset_train = datasets.ImageFolder('./poisoned_cifar10_pngs/train', transform=trans_cifar)
         dataset_test = datasets.ImageFolder('./poisoned_cifar10_pngs/test', transform=trans_cifar)
-
         if args.iid:
             dict_users = cifar_iid(dataset_train, args.num_users)
         else:
             exit('Error: only consider IID setting in CIFAR10')
+    else:
+        exit('Error: unrecognized dataset')
+    img_size = dataset_train[0][0].shape
 
-        train(net_glob, args, dataset_train)
+    # Training Poisoned ###########################################################################
+    net_glob.train()
+    # copy weights
+    w_glob = net_glob.state_dict()
+    loss_train = []
+    cv_loss, cv_acc = [], []
+    val_loss_pre, counter = 0, 0
+    net_best = None
+    best_loss = None
+    val_acc_list, net_list = [], []
 
-        test(net_glob, args, dataset_train, dataset_test)
-        
+    if args.all_clients: 
+        print("Aggregation over all clients")
+        w_locals = [w_glob for i in range(args.num_users)]
+    for iter in range(args.epochs):
+        loss_locals = []
+        if not args.all_clients:
+            w_locals = []
+        m = max(int(args.frac * args.num_users), 1)
+        idxs_users = np.random.choice(range(args.num_users), m, replace=False)
+        for idx in idxs_users:
+            local = LocalUpdate(args=args, dataset=dataset_train, idxs=dict_users[idx])
+            w, loss = local.train(net=copy.deepcopy(net_glob).to(args.device))
+            if args.all_clients:
+                w_locals[idx] = copy.deepcopy(w)
+            else:
+                w_locals.append(copy.deepcopy(w))
+            loss_locals.append(copy.deepcopy(loss))
+        # update global weights
+        w_glob = FedAvg(w_locals)
 
+        # copy weight to net_glob
+        net_glob.load_state_dict(w_glob)
+
+        # print loss
+        loss_avg = sum(loss_locals) / len(loss_locals)
+        print('Round {:3d}, Average loss {:.3f}'.format(iter, loss_avg))
+        loss_train.append(loss_avg)
+
+    # Testing Poisoned ############################################################################
+    net_glob.eval()
+    acc_train, loss_train = test_img(net_glob, dataset_train, args)
+    acc_test, loss_test = test_img(net_glob, dataset_test, args)
+    print("Training accuracy: {:.2f}".format(acc_train))
+    print("Testing accuracy: {:.2f}".format(acc_test))
